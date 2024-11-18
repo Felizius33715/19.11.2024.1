1b87693129b6c36d256d2a8db27195d9274a07ac
Felizius <Felizius@yandex.ru>
2024-11-19 01:45:24 +0700

    Добавьте файлы проекта.

---
+++ 19.11.2024.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+void countAndSortCharacters(const std::string& text) {
+    std::map<char, int> frequency;
+
+    for (char c : text) {
+        frequency[c]++;
+    }
+
+    std::vector<std::pair<char, int>> freqVector(frequency.begin(), frequency.end());
+
+    std::sort(freqVector.begin(), freqVector.end(),[](const auto& a, const auto& b) { return a.second > b.second; });
+
+    for (const auto& pair : freqVector) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+}
+
+int main() {
+    std::string inputText;
+    std::getline(std::cin, inputText);
+    countAndSortCharacters(inputText);
+    return 0;
+}
